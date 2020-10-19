b7b75fa42e4796145ac833fa22148f34823a6246
Christan Ariel Salazar Verduga <santisimoariel@gmail.com>
2020-10-20 01:19:36 +0200

    Creando un interceptor schedule

---
+++ src/main/java/com/bolsadeideas/springboot/scheduleinterceptor/app/interceptors/ScheduleInterceptor.java
@@ -0,0 +1,52 @@
+package com.bolsadeideas.springboot.scheduleinterceptor.app.interceptors;
+
+import java.util.Calendar;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+public class ScheduleInterceptor implements HandlerInterceptor {
+	@Value("${config.schedule.open}")
+	private Integer open;
+	@Value("${config.schedule.close}")
+	private Integer close;
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// lo primero es obtener la hora actual
+		Calendar calendar = Calendar.getInstance();
+		int hour = calendar.get(Calendar.HOUR_OF_DAY);
+		if (hour >= open && hour <= close) {
+			StringBuilder message = new StringBuilder("Wellcome to  opening hours for clients");
+			message.append(", we attend from ");
+			message.append(open);
+			message.append(" hrs. ");
+			message.append("to ");
+			message.append(close);
+			message.append(" hrs. ");
+			message.append(" Thank you for visiting us.");
+			// la idea es pasar el mensaje a los atributos del request
+			request.setAttribute("message", message.toString());
+			return true;
+		}
+		// si es falso manejamos el response si es falso reenviamos a otra página con el
+		// context pat h
+		response.sendRedirect(request.getContextPath().concat("/closed"));
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		String message = (String) request.getAttribute("message");
+		modelAndView.addObject("message", message);
+	}
+
+}
